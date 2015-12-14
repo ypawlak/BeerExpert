61b9ba4abf0234b63e1ee99173ccd80a4c9689db
Piotr Markowski <anakin_1994@op.pl>
2015-12-14 23:55:40 +0100

    Dodawanie obiektów do sesji

--- BeerExpert/src/main/java/gui/DialogWindowController.java
+++ BeerExpert/src/main/java/gui/DialogWindowController.java
@@ -8,6 +8,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.RadioButton;
@@ -17,6 +18,7 @@ import javafx.scene.control.ToggleGroup;
 
 import com.beerExpert.Question;
 import com.beerExpert.BeerExpertMain;
+import com.beerExpert.UserTaste;
 
 public class DialogWindowController extends Observable implements Initializable {
 
@@ -26,28 +28,38 @@ public class DialogWindowController extends Observable implements Initializable 
 	private VBox mainBox;
 	@FXML
 	private Button okBtn;
-	
+
 	public void initialize(URL arg0, ResourceBundle arg1) {
 
 	}
-	
-	public void ShowQuestion (Question question) {
+
+	public void ShowQuestion(Question question) {
 		questionLbl.setText(question.QuestionTxt);
 		ToggleGroup questionGroup = new ToggleGroup();
-		for(Map.Entry <String, Object> ans : question.Answers.entrySet()) {
+		for (Map.Entry<String, Object> ans : question.Answers.entrySet()) {
 			RadioButton option = new RadioButton(ans.getKey());
 			option.setToggleGroup(questionGroup);
 			option.setUserData(ans);
 			mainBox.getChildren().add(option);
 		}
 	}
-	
-	 @FXML
-	 private void okButtonClicked(ActionEvent event) {
-	     setChanged();
-         notifyObservers();
-         BeerExpertMain.getKsession().fireAllRules();
-         ShowQuestion(BeerExpertMain.ActiveQuestion);
-	 }
+
+	@FXML
+	private void okButtonClicked(ActionEvent event) {
+		setChanged();
+		notifyObservers();
+		for (Node child : mainBox.getChildren()) {
+			if (child instanceof RadioButton
+					&& ((RadioButton) child).isSelected()) {
+				BeerExpertMain.getKsession().insert(
+						BeerExpertMain.ActiveQuestion.Answers
+								.get(((RadioButton) child).getText()));
+
+			}
+		}
+		BeerExpertMain.getKsession().fireAllRules();
+		ShowQuestion(BeerExpertMain.ActiveQuestion);
+
+	}
 
 }
