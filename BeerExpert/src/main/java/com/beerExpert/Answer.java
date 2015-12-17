b8b2e81769852165ded8338e29f4a983976abc39
jpawlak <twojlech@gmail.com>
2015-12-17 13:49:12 +0100

    Wyświetlanie propozycji piwa

--- BeerExpert/src/main/java/com/beerExpert/Answer.java
+++ BeerExpert/src/main/java/com/beerExpert/Answer.java
@@ -4,22 +4,20 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+import javafx.scene.image.Image;
+
 import javax.imageio.ImageIO;
 
 public class Answer {
 	public String AnswerTxt;
-	public BufferedImage AnswerImage;
+	public Image AnswerImage;
+	public Question AdditionalQuestion;
 	private final String imgPath = "src\\images\\";
 	
 	public void SetImage(String name){
 		String fullPath = imgPath + name;
-		BufferedImage img = null;
-		try {
-		    img = ImageIO.read(new File("src\\images\\Eurolager.jpg"));
-		    AnswerImage = img;
-		} catch (IOException e) {
-			System.out.println("Nie znaleziono obrazu!");
-		}
+		File imFile = new File(fullPath);
+		AnswerImage = new Image(imFile.toURI().toString());
 	}
 
 }
--- BeerExpert/src/main/java/gui/DialogWindowController.java
+++ BeerExpert/src/main/java/gui/DialogWindowController.java
@@ -16,9 +16,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Button;
 import javafx.scene.control.Toggle;
@@ -33,21 +37,44 @@ public class DialogWindowController implements Initializable {
 	@FXML
 	private Label questionLbl;
 	@FXML
-	private GridPane mainPane;
+	private Label beerAnswerLbl;
+	@FXML
+	private ImageView answerImage;
+	@FXML
+	private VBox mainPane;
+	@FXML
+	private VBox answerBox;
+	@FXML
+	private GridPane questionGrid;
 	@FXML
 	private VBox optionsBox;
 	@FXML
 	private Button okBtn;
+	
+	private final double AnswerHeight = 430;
+	private final double AnswerWidth = 400;
 
 	public void initialize(URL arg0, ResourceBundle arg1) {
-
+		answerBox.managedProperty().bind(answerBox.visibleProperty());
+		questionGrid.managedProperty().bind(questionGrid.visibleProperty());
+		beerAnswerLbl.managedProperty().bind(beerAnswerLbl.visibleProperty());
+		answerImage.managedProperty().bind(answerImage.visibleProperty());
+		answerBox.setVisible(false);
+		beerAnswerLbl.setFont(Font.font("Verdana", FontWeight.BOLD, 20));
 	}
 	
 	public void ShowQuestion () {
 		optionsBox.getChildren().clear();
 		if (BeerExpertMain.SelectedAnswer != null){
-			questionLbl.setText(BeerExpertMain.SelectedAnswer.AnswerTxt);
-			//TODO: wyœwietliæ AnwerImage
+			questionGrid.setVisible(false);
+			answerBox.setVisible(true);
+			beerAnswerLbl.setText(BeerExpertMain.SelectedAnswer.AnswerTxt);
+			answerImage.setImage(BeerExpertMain.SelectedAnswer.AnswerImage);
+			answerImage.setPreserveRatio(true);
+			answerImage.setFitHeight(AnswerHeight - 130);
+			answerImage.getScene().getWindow().setHeight(AnswerHeight);
+			answerImage.getScene().getWindow().setWidth(AnswerWidth);
+			
 		}
 		else{
 			if(BeerExpertMain.ActiveQuestion == null)
